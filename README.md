98b1cf7dcffc4981746853774da793553a4f883d
Wali, Prasad <pwali@ebay.com>
2017-11-12 09:20:34 -0800

    Refactor: Moved load logic outside of client

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # handlebars
 http://handlebarsjs.com/
 
-
+# To complie handlebar templates
+handlebars src/templates/hello.handlebars  -f src/js/hello.handlebars.js -c
--- src/js/my.js
+++ src/js/my.js
@@ -1,10 +1,19 @@
-var context = {
-  title: "My New Post",
-  body: "This is my first post!"
-};
-var innerHtml = Handlebars.templates.hello(context);
-console.log(innerHtml);
+(function() {
+  // Load my handlebar template
+  script = document.createElement('script');
+  script.src = "../../js/hello.handlebars.js";
+  document.body.appendChild(script);
+})();
 
-$(document).ready(function() {
-  $("#hello-handler").html(innerHtml);
-});
+var context = function() {
+  return {
+    title: "My New Post",
+    body: "This is my first post!"
+  };
+};
+
+var getInnerHtml = function(id) {
+  var innerHtml = Handlebars.templates.hello(context());
+  console.log(innerHtml);
+  return innerHtml;
+};
