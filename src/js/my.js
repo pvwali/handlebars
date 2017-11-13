58c91f5175f1f4af32b52f5744ef0873866caee1
Wali, Prasad <pwali@ebay.com>
2017-11-12 22:46:29 -0800

    Added another banner with image

--- src/js/my.js
+++ src/js/my.js
@@ -1,7 +1,11 @@
 (function() {
   // Load my handlebar template
+  // script = document.createElement('script');
+  // script.src = "../../js/hello.handlebars.js";
+  // document.body.appendChild(script);
+
   script = document.createElement('script');
-  script.src = "../../js/hello.handlebars.js";
+  script.src = "../../js/banner.handlebars.js";
   document.body.appendChild(script);
 })();
 
@@ -12,8 +16,20 @@ var context = function() {
   };
 };
 
-var getInnerHtml = function(id) {
+var getInnerHtml = function() {
   var innerHtml = Handlebars.templates.hello(context());
+  console.log(innerHtml);
+  return innerHtml;
+};
+
+var bannerContext = function() {
+  return {
+    headline: "Awesome Phones !!",
+    image: "https://i.ebayimg.com/00/s/NTgxWDE2MDA=/z/TGAAAOSwk~ZaBQU1/$_57.JPG"
+  };
+};
+var getInnerHtmlAdBanner = function() {
+  var innerHtml = Handlebars.templates.banner(bannerContext());
   console.log(innerHtml);
   return innerHtml;
 };
